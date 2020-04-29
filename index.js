79113b4b108e7165758dfdc4a57245807a6333d3
hoani <hoani.bryson@gmail.com>
2020-04-29 16:05:08 +1200

    Got working with local cennznet node

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const PlugRuntimeTypes = require('@plugnet/plug-api-types');
+const { Api } = require('@cennznet/api');
 const testingPairs = require('@polkadot/keyring/testingPairs');
-const { stringToU8a, u8aToHex } = require('@polkadot/util');
 
 if (require.main === module) {
   run("ws://localhost:9944")
@@ -17,14 +15,8 @@ async function run(address) {
   console.log(`Connecting to ${address}`);
 
   // Create the API and wait until ready
-  const provider = new WsProvider(address);
-  const api = await ApiPromise.create({
-    provider,
-    types: {
-      ...PlugRuntimeTypes.default,
-      AttestationTopic: 'U256',
-      AttestationValue: 'U256',
-     }
+  const api = await Api.create({
+    provider: address
   });
 
   // Retrieve the chain & node information information via rpc calls
@@ -35,40 +27,5 @@ async function run(address) {
   ]);
 
   console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
-
-  // Get the testing pairs
-  const keyring = testingPairs.default({ type: 'ed25519'});
-
-  let [alice_balance, bob_balance] = await Promise.all([
-    api.query.balances.account(keyring.alice.address),
-    api.query.balances.account(keyring.bob.address),
-  ]);
-
-  console.log(`Alice's balance: ${alice_balance.free}`);
-  console.log(`Bob's balance: ${bob_balance.free}`);
-
-  console.log(`Alice is transferring 12345 to Bob`);
-
-  console.log(keyring.alice.sign());
-
-  // Sign and send a transfer from Alice to Bob
-  // const txHash = await api.tx.attestation.setClaim(keyring.bob.address, '0x11', '65536')
-  //   .signAndSend(keyring.alice);
-  // // Sign and send a transfer from Alice to Bob
-  // const txHash = await api.tx.balances
-  //   .transfer(keyring.bob.address, 12345)
-  //   .signAndSend(keyring.alice);
-
-  // Show the hash
-  //console.log(`Transaction submitted with hash ${txHash}`);
-
-  // Retrieve the chain & node information information via rpc calls
-  // [alice_balance, bob_balance] = await Promise.all([
-  //   api.query.attestation.values(keyring.alice.address, keyring.bob.address, '0x0000000000000000000000000000000000000000000000000000000000000100'),
-  //   api.query.balances.account(keyring.bob.address),
-  // ]);
-
-  console.log(`Alice's balance: ${alice_balance}`);
-  console.log(`Bob's balance: ${bob_balance.free}`);
 }
 
