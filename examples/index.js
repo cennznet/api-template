67f0adcee7768aca3dd9843482b1d6fb2de83bd4
Roy Yang <roy.yang@centrality.ai>
2020-05-05 16:30:58 +1200

    Added a bunch of functions in the cennz x spot exchange module

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,9 @@
 //Imports functions from other files
 const { reportBalances, transfer } = require('./generic_asset.js');
+const { addLiquidity, removeLiquidity, liquidityValue, liquidityPrice, buyPrice, sellPrice, buyAsset, sellAsset}
+	= require('./cennz_x_spot_exchange.js');
 
+//Import other dependencies
 const { Api } = require('@cennznet/api');
 const testingPairs = require('@polkadot/keyring/testingPairs');
 const keyring = testingPairs.default({ type: 'sr25519'});
@@ -16,16 +19,45 @@ if (require.main === module) {
 }
 
 async function parseCommand(settings, api){
-	console.log(`${settings}`);
+	console.log(`>>>${settings.command}`)
 	switch(settings.command) {
+		//Generic Asset
 		case `transfer`:
 			await transfer(keyring, api, settings.amount, settings.asset_id);
 			break;
 		case `balances`:
 			await reportBalances(keyring, api, settings.asset_id);
 			break;
+
+		//Cennz X Spot Exchange: Liquidity
+		case 'add_liquidity':
+			await addLiquidity(keyring, api, settings.asset_id, settings.asset_amount, settings.core_amount);
+			break;
+		case 'remove_liquidity':
+			await removeLiquidity(keyring, api, settings.asset_id, settings.amount, settings.min_trade_asset_amount, settings.min_core_asset_amount);
+			break;
+		case 'liquidity_value':
+			await liquidityValue(keyring, api, settings.account, settings.asset_id);
+			break;
+		case 'liquidity_price':
+			await liquidityPrice(keyring, api, settings.asset_id, settings.amount);
+			break;
+
+		//Cennz X Spot Exchange: Asset Exchange
+		case 'buy_price':
+			await buyPrice(keyring, api, settings.asset_to_buy, settings.buy_amount, settings.asset_to_sell);
+			break;
+		case 'sell_price':
+			await sellPrice(keyring, api, settings.asset_to_sell, settings.sell_amount, settings.asset_to_buy);
+			break;
+		case 'buy_asset':
+			await buyAsset(keyring, api, settings.recipient, settings.asset_to_sell, settings.asset_to_buy, settings.buy_amount);
+			break;
+		case 'sell_asset':
+			await sellAsset(keyring, api, settings.recipient, settings.asset_to_sell, settings.asset_to_buy, settings.sell_amount);
+			break;
 		default:
-			help();
+			break;
 	}
 }
 
